34bac527e1624218624020306db6bd26f5640a69
Nathan <naslee2@gmail.com>
2018-08-19 21:01:19 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Radium, {StyleRoot} from 'radium';
+import Person from './Person/Person';
+
 
 class App extends Component {
   state = {
@@ -46,36 +48,56 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover':{
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
   };
 
   let persons = null;
 
   if (this.state.showPersons) {
     persons = (
+
       <div>
         {this.state.persons.map((person, index) => {
           return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id} changed={(event) => this.nameChangedHandler(event, person.id)}/>
         })}
-        
       </div> 
     );
+    style.backgroundColor ='red';
+    style[':hover'] = {
+      backgroundColor: 'salmon',
+      color: 'black'
+    };
   }
 
+    const classes = [];
+    if (this.state.persons.length <= 2){
+      classes.push('red'); //classes = ['red']
+    }
+    if (this.state.persons.length <= 1){
+      classes.push('bold'); //classes = ['red', 'bold']
+    }
+
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I am a cat</h1>
-        <p>Cat is working</p>
+        <p className={classes.join(' ')}>Cat is working</p>
         <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button> 
       {persons}
       </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'cats work now'));
   }
 }
 
-export default App;
+export default Radium(App);
